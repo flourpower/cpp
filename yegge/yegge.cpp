8b47ba763c5c804b39213e9515b4ab3f82dd3e66
Augustus Odena <aqo2@columbia.edu>
2014-03-29 14:36:13 -0400

    there ended up being more than 5 of these, but i think I did them all

--- yegge/yegge.cpp
+++ yegge/yegge.cpp
@@ -1,6 +1,9 @@
 #include <string>
 #include <iostream>
 #include <stdio.h>
+#include <fstream>
+#include <stdlib.h>
+#include <vector>
 using namespace std;
 
 //Answers to Steve Yegge's 5 phone screen coding questions in C++
@@ -45,6 +48,40 @@ void tables(){
 	}
 }
 
+int summy(fstream & f){
+	if (f.is_open()){
+		string line;
+		int sum;
+		while (! f.eof()){
+			getline(f,line);
+			sum += atoi(line.c_str());
+		}
+		f.close();
+		return sum;
+	}
+	else{
+		return 0;
+	}
+}
+
+void odds(){
+	int i;
+	for(i = 1; i < 100; i++){
+		if ( (i % 2) == 1 )
+			printf("%d\n", i);
+	}
+}
+
+int big(vector<int> & v){
+	//find the largest int value in an int array
+	int answer = v.at(0);
+	int i;
+	for(i = 0; i < v.size(); i++){
+		if (v.at(i) > answer)
+			answer = v.at(i);
+	}
+	return answer;
+}
 
 int main(int argc, char** argv){
 	string str = "my name is augie";
@@ -53,4 +90,15 @@ int main(int argc, char** argv){
 	cout << str << endl;
 	cout << "fibonacci numbers: " << endl << fib(1) << endl << fib (2) << endl << fib(3) << endl << fib(4) << endl << fib(5) << endl << fib(6) << endl;
 	tables();
+	fstream f (argv[1]);
+	cout << endl << "sum of ints in specified file is:" << summy(f) << endl;
+	cout << "odds from 1 to 99:" << endl;
+	odds();
+	vector<int> v;
+	v.push_back(7);
+	v.push_back(4);
+	v.push_back(9);
+	v.push_back(71);
+	v.push_back(1);
+	cout << "largest element of the vector was: " << big(v) << endl;
 }
